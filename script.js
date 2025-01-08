27cde209b929009a622d5e1887146c96359d267d
mouhamed0001 <tanjirobouraib003@gmail.com>
2025-01-08 12:54:03 +0100

    changes in html files, adding top & right bar to js file

---
+++ script.js
@@ -0,0 +1,18 @@
+document.write('<header>\
+            <img src="imgs/download.jpg">\
+            <h1>محمد بورايب</h1>\
+        </header>\
+        <nav class="rightbar">\
+            <div>\
+                <a href="#" class="active"><i class="fa-solid fa-question"></i></a>\
+                <span id="tooltip">من هو محمد بورايب؟</span>\
+            </div>\
+            <div>\
+                <a href="socials.html"><i class="fa-solid fa-user"></i></a>\
+                <span id="tooltip">حساباتي على مواقع التواصل!</span>\
+            </div>\
+            <div>\
+                <a href="infos.html"><i class="fa-solid fa-code" style="color: hsl(47, 100%, 50%);"></i></a>\
+                <span id="tooltip">خبراتي في مجال البرمجة</span>\
+            </div>\
+        </nav>');
